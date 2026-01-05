86b3d29d69d364626f61d2cf66b7961e4e0692cd
Alex Costa <alex.costa@doinstruct.com>
2026-01-05 16:16:22 +0100

    chore: add autodocs tags

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,15 @@
 import type { Preview } from '@storybook/sveltekit'
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	tags: ['autodocs'],
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	}
 };
 
 export default preview;
