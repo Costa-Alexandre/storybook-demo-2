7fe77e1a95b8a41fbd088e9a6c9fed2f4f939828
Alex Costa <alex.costa@doinstruct.com>
2026-01-05 17:57:59 +0100

    chore: remove disable

--- src/stories/Page.stories.svelte
+++ src/stories/Page.stories.svelte
@@ -11,7 +11,6 @@
     parameters: {
       // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
       layout: 'fullscreen',
-      chromatic: { disableSnapshot: true },
     },
   });
 </script>
