2e22d311ef68694ff4fb91fd5580a17815740788
Alex Costa <alex.costa@doinstruct.com>
2026-01-06 16:10:05 +0100

    feat: add nested storybook

---
+++ apps/manager/src/stories/Button.stories.svelte
@@ -0,0 +1,26 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+	import { fn } from 'storybook/test';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Example/Button',
+		component: Button,
+		tags: ['autodocs'],
+		argTypes: {
+			backgroundColor: { control: 'color' },
+			size: {
+				control: { type: 'select' },
+				options: ['large']
+			}
+		},
+		args: {
+			onclick: fn()
+		}
+	});
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+
+<Story name="Large" args={{ size: 'large', label: 'Button' }} />
