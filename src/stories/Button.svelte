4dc38411483d85dceb5123627b0a6c96c1856a91
Alex Costa <alex.costa@doinstruct.com>
2026-02-12 14:09:52 +0100

    change label color

--- src/stories/Button.svelte
+++ src/stories/Button.svelte
@@ -39,7 +39,7 @@
 	}
 	.storybook-button--primary {
 		background-color: #3c3c3c;
-		color: #0b0b0b;
+		color: #ffffff;
 	}
 	.storybook-button--secondary {
 		box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
